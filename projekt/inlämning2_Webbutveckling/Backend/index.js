3bf078568adce2cccd8dba432d8460f9f312574a
Gabriel Karlsson Gonzalez <el_sueco91@hotmail.com>
2020-06-06 17:21:30 +0200

    post WIP

--- projekt/inlämning2_Webbutveckling/Backend/index.js
+++ projekt/inlämning2_Webbutveckling/Backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyparser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 4000
 const fs =require('fs')
 const path= require('path')
 app.use(bodyparser.json())
@@ -14,6 +14,7 @@ app.post('/Login', (req,res)=>{
 if( req.body.username=== 'test' & req.body.pw==='1234'){
     fs.readFile('users.json',(err, data)=>{
         if(err) throw err;
+        
         let users =JSON.parse(data);
         //return res.JSON(users);
         console.log(users);
@@ -21,8 +22,13 @@ if( req.body.username=== 'test' & req.body.pw==='1234'){
 }})
 
 app.post('/reg', (req,res)=>{
-    fs.writeFile('users.json', (err,data)=> {
-        if(err) throw err;
-        let users =req.body.username & req.body.pw & req.body.subbed})
+    fs.readFile('users.json', (err, data)=>{
+        if (err) throw err
+        var userArr =JSON.parse(data)
     })
+    fs.writeFile('./users.json', JSON.stringify(userArr), 'utf-8', function(err) {
+		if (err) throw err
+		console.log('Done!')
+    })
+})
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- projekt/inlämning2_Webbutveckling/Frontend/client/src/index.js
+++ projekt/inlämning2_Webbutveckling/Frontend/client/src/index.js
@@ -1,17 +1,71 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ReactDOM, { render } from 'react-dom';
+import './App.css';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email:'',
+      password:'',
+      subbed: true
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.name === 'subbed' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  };
+  handleSubmit(event){
+    const {email, password, subbed}=this.state;
+    fetch("localhost:1000/reg", {
+      method:'POST',
+      headers:{
+        'Content-type': 'application/json'
+      }, 
+      body: JSON.stringify(this.state)
+    })
+    .then((response)=>response.json())
+    .then((result)=> {
+      console.log(result)
+    })
+  }
+
+
+render(){
+  
+  const {email, password, subbed}=this.state;
+  return(
+    <header className="App-header">
+        <form method="POST">
+
+        <label>E-post:</label><br/><input name="email" type="text" value={this.state.email}onChange={this.handleInputChange}/><br/>
+        <label>Lösenord</label><br/><input name="password" type="password" value={this.state.password}onChange={this.handleInputChange}/><br/>
+        <label>Vill du ta del av vårt nyhetsbrev?</label><input name="subbed" id="sub" type="checkbox" checked={this.state.subbed} onChange={this.handleInputChange}/><br/>
+        <input id="submit" type="submit"></input>
+        </form></header>
+  );
+}
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    <Register />
+,
   document.getElementById('root')
-);
+  );
+  
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
